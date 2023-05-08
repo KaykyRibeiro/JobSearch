396236e5f03bf138727a03ec0e34df1f806371b0
Kayky Ribeiro <kaykykaro04@gmail.com>
2023-05-08 19:21:07 -0300

    arrumando cagada parte 2

--- WebInicial/cadastro.php
+++ WebInicial/cadastro.php
@@ -10,7 +10,7 @@ include_once('conexao.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="./Estilos/estilos.css">
-    <link rel="stylesheet" href="./Estilos/estilos2.css">
+    <link rel="stylesheet" href="./Estilos/estilo2.css">
     
 </head>
 
