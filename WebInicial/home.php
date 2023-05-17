816477151e35329553ca2b8bb6a142efd64260b9
Kayky Ribeiro <kaykykaro04@gmail.com>
2023-05-17 18:35:23 -0300

    17-05-2023
    responsividade e mais b.o

--- WebInicial/home.php
+++ WebInicial/home.php
@@ -1,14 +1,4 @@
-<?php
-session_start();
-include_once('conexao.php');
 
-if (!isset($_SESSION['login'])) {
-    header("location: login.php");
-}
-
-$anuncio = "SELECT * FROM vagas WHERE vag_id = 1";
-$post = $mysqli->query($anuncio);
-?>
 <!DOCTYPE html>
 <html lang="pt-bt">
 
@@ -16,14 +6,16 @@ $post = $mysqli->query($anuncio);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Estilos/estilopadrao.css">
-    <link rel="stylesheet" href="./Estilos/estilo3_h.css">
+    <link rel="stylesheet" href="estilo3_h.css">
+    <link rel="stylesheet" href="./estilos/estilopadrao.css">
+
+
     <title>JobSearch</title>
 </head>
 
 <body>
     <nav>
-
+    <a class="select" id="logo" href=""><img class="icon" src="./imagens/config.png" alt=""></a>
         <a class="selecionado" href=""><img class="icon" src="./imagens/home_azul.png" alt=""></a>
         <a class="select" href="chat.php"><img class="icon" src="./imagens/chat.png" alt="" /></a>
         <button id="notificacao" class="select">
@@ -35,29 +27,41 @@ $post = $mysqli->query($anuncio);
     <main>
         <h2>Postagens</h2>
         <div class="quadro">
-            <?php
-            while ($row_produto2 = mysqli_fetch_assoc($resultado_produto2)) {
-                $_SESSION['livroId'] = $row_produto2['livId'];
+            <!--<?php
+           // while ($row_produto2 = mysqli_fetch_assoc($resultado_produto2)) {
+              //  $_SESSION['livroId'] = $row_produto2['livId'];
             ?>
-                <a href="livros.php?id=<?php echo $row_produto2['livId']; ?>">
+                <a href="livros.php?id=<?php //echo $row_produto2['livId']; ?>">
                     <div class="postagem">
 
-                        <img src="<?php echo $row_produto2['livImg'] ?>" class="card-img-top" alt="...">
+                        <img src="<?php //echo $row_produto2['livImg'] ?>" class="card-img-top" alt="...">
 
                         <h1>
-                            <?php echo $row_produto2['livTitulo'] ?>
+                            <?php //echo $row_produto2['livTitulo'] ?>
                         </h1>
                         <p>
-                            <?php echo $row_produto2['vagdescricao'] ?>
+                            <?php //echo $row_produto2['vagdescricao'] ?>
                         </p>
-                    <?php }
-                    ?>
+            <?php //}
+            ?>
+
+
+
+                        <h1>Emprego</h1>
+                        <p>Lore ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vitae laborum accusantium mollitia modi harum voluptate deleniti placeat asperiores delectus porro, dignissimos a sunt saepe qui voluptatem natus! Non, illo.</p>
                     </div>
-                </a>
+                </a>-->
+                <div class="postagem">
+                <h1>Emprego</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vitae laborum accusantium mollitia modi harum voluptate deleniti placeat asperiores delectus porro, dignissimos a sunt saepe qui voluptatem natus! Non, illo.</p>
+            </div>
         </div>
+        <input type="button" value="Recaregar pagina" onclick="recaregar()">
     </main>
 
-    <script src="./JS/jspadrao.js"></script>
+    <script src="./js/jspadrao.js"></script>
+
+
 </body>
 
 </html>
