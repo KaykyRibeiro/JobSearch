35337e852bd97582310deb080cc8cf0f6d1d1039
Kayky Ribeiro <kaykykaro04@gmail.com>
2023-05-29 18:41:41 -0300

    landing_page
    terminando landing_page

---
+++ WebInicial/JS/cadescolha.js
@@ -0,0 +1,17 @@
+
+
+const div = document.querySelector("div.escolhas")
+function escolha(){
+    var cadativo = document.getElementById('btncad')
+    if(cadativo.classList.contains('cadativo')){
+        cadativo.classList.remove("cadativo")
+        cadativo.classList.add("cadastro")
+        div.innerHTML = ''
+    }else{
+        div.innerHTML += `<input class="escolha" type="button" value="EMPRESA">`
+        div.innerHTML += `<input class="escolha" type="button" value="EMPRESA">`
+        cadativo.classList.remove("cadastro")
+        cadativo.classList.add("cadativo")
+    }
+    
+}
