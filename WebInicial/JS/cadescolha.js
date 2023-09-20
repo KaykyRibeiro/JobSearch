93a4da719010124345e27421a8cd9274d348e63e
Kayky Ribeiro <kaykykaro04@gmail.com>
2023-09-20 18:10:32 -0300

    aaaaaa

--- WebInicial/JS/cadescolha.js
+++ WebInicial/JS/cadescolha.js
@@ -8,8 +8,8 @@ function escolha() {
         cadativo.classList.add("cadastro")
         div.innerHTML = ''
     } else {
-        div.innerHTML += `<input class="escolha" type="button" value="EMPRESA" id="empresa" onclick="msgEmpresa()">`
-        div.innerHTML += `<input class="escolha" type="button" value="CANDIDATO" id="candidato" onclick="msgCandidato()">`
+        div.innerHTML += ` <a href="cadastro_empresa.php"><input class="escolha" type="button" value="EMPRESA" id="empresa" onmouseenter="msgEmpresa()" onmouseout="msgEmpresaSair()"></a>`
+        div.innerHTML += `<a href="cadastro_candidato.php"><input class="escolha" type="button" value="CANDIDATO" id="candidato" onmouseenter="msgCandidato()" onmouseout="msgCandidatoSair()"></a>`
         cadativo.classList.remove("cadastro")
         cadativo.classList.add("cadativo")
         const empresa = window.document.getElementById('empresa')
@@ -21,49 +21,45 @@ function escolha() {
 }
 function msgEmpresa() {
     const divMessage = document.querySelector("div.escolhas")
-    if (empresa.classList.contains('empAtivo')) {
-        empresa.classList.remove("empAtivo")
-        empresa.classList.add("emp")
-        ativNotif = false
-        var destroi = document.querySelector("div.msgEmpresa")
-        destroi.parentNode.removeChild(destroi)
+        
 
-
-    } else {
         empresa.classList.remove("emp")
         empresa.classList.add("empAtivo")
         const message = document.createElement("div")
         message.classList.add("msgEmpresa")
         message.innerHTML = `<h1>Empresa</h1>
                              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum molestiae, eos praesentium nihil consequatur fuga cumque aliquam aperiam, est odit labore ratione recusandae ducimus architecto dolor explicabo! Tenetur, est molestias?</p>
-                             <br>
-                             <a class="cadastro" href="cadastro_empresa.php">CADASTRE AGORA!</a>`
+                             <br>`
         divMessage.appendChild(message)
         var destroi = document.querySelector("div.msgCandidato")
         destroi.parentNode.removeChild(destroi)
-    }
+    
+}
+function msgEmpresaSair(){
+        empresa.classList.remove("empAtivo")
+        empresa.classList.add("emp")
+        ativNotif = false
+        var destroi = document.querySelector("div.msgEmpresa")
+        destroi.parentNode.removeChild(destroi)
 }
 function msgCandidato() {
     const divMessage = document.querySelector("div.escolhas")
-    if (candidato.classList.contains('canAtivo')) {
-        candidato.classList.remove("canAtivo")
-        candidato.classList.add("can")
-        ativNotif = false
-        var destroi = document.querySelector("div.msgCandidato")
-        destroi.parentNode.removeChild(destroi)
-
-
-    } else {
         candidato.classList.remove("can")
         candidato.classList.add("canAtivo")
         const message = document.createElement("div")
         message.classList.add("msgCandidato")
         message.innerHTML = `<h1>Candidato</h1>
                              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum molestiae, eos praesentium nihil consequatur fuga cumque aliquam aperiam, est odit labore ratione recusandae ducimus architecto dolor explicabo! Tenetur, est molestias?</p>
-                             <br>
-                             <a class="cadastro" href="cadastro_candidato.php">CADASTRE AGORA!</a>`
+                             <br>`
         divMessage.appendChild(message)
         var destroi = document.querySelector("div.msgEmpresa")
         destroi.parentNode.removeChild(destroi)
-    }
+    
+}
+function msgCandidatoSair(){
+        candidato.classList.remove("canAtivo")
+        candidato.classList.add("can")
+        ativNotif = false
+        var destroi = document.querySelector("div.msgCandidato")
+        destroi.parentNode.removeChild(destroi)
 }
