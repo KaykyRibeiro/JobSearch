7afc108bd214603f443c2065749071e68ef7bda2
RanaruKhz <ramyreslopes2@hotmail.com>
2023-11-23 20:51:31 -0300

    validação pronta

--- WebInicial/validar_cadastro.php
+++ WebInicial/validar_cadastro.php
@@ -50,18 +50,20 @@ if ($_POST) {
       exit; // Saia do script
    }
 
-   $consulta = "SELECT usu_cpf FROM tbl_usuario";
+   $consulta = "SELECT * FROM tbl_usuario WHERE usu_cpf = :cpf";
    $stmtConsulta = $conexao->prepare($consulta);
+   $stmtConsulta->bindParam(':cpf', $cpf); // Vinculando o valor de $cpf ao parâmetro da consulta
    $stmtConsulta->execute();
    $rowConsulta = $stmtConsulta->fetch(PDO::FETCH_ASSOC);
-   if($rowConsulta['usu_cpf'] = $cpf){
+   
+   if($rowConsulta){
       echo "CPF já cadastrado.";
    }
    else{
 
    // Inserir dados na tabela tbl_usuario[]
    try{
-   $sqlInserir = "INSERT INTO tbl_usuario (usu_nome, usu_sobrenome, usu_email, usu_senha, usu_telefone, usu_cpf, usu_dataNasc, usu_logradouro, usu_numRua, usu_complemento, usu_bairro, usu_cidCodigo, usu_ufeCodigo, usu_cep, usu_sexo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+   $sqlInserir = "INSERT INTO tbl_usuario (usu_nome, usu_sobrenome, usu_email, usu_senha, usu_telefone, usu_cpf, usu_dataNasc, usu_logradouro, usu_numRua, usu_complemento, usu_bairro, usu_cidCodigo, usu_ufeCodigo, usu_cep, usu_sexo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
    $stmtInserir = $conexao->prepare($sqlInserir);
    $stmtInserir->execute([$usu, $sobrenome, $email, $senha, $telefone, $cpf, $datanas, $logradouro, $numero, $complemento, $bairro, $codigoCidade, $codigoEstado,$cep, $sexo]);
    header('Location: login.php');
@@ -69,7 +71,9 @@ if ($_POST) {
    catch(PDOException $e){
       die("Erro ao Inserir os Dados Fornecidos: " . $e->getMessage());
    }
+   exit;
 
 }
+
 }
 ?>
